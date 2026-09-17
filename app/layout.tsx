a8395f20bfa336e15e839dfca470e557b7abd424
Noelle Dang <dang.noelle@gmail.com>
2026-09-16 23:50:19 -0700

    Update wedding site titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,61 +1,15 @@
-import WelcomeScreen from "./WelcomeScreen";
-import type { Metadata } from "next";
-import {
-  Cormorant_Garamond,
-  Allura,
-  Great_Vibes,
-  Montserrat,
-} from "next/font/google";
-import "./globals.css";
+import { headers } from "next/headers";
 
-import Navbar from "./components/Navbar";
-import MusicPlayerWrapper from "./MusicPlayerWrapper";
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers();
+  const host = headersList.get("host") || "";
 
-import { LanguageProvider } from "./LanguageProvider";
-import LanguageSwitcher from "./LanguageSwitcher";
+  const isVietnameseSite = host.includes("tancuong-lamnghi");
 
-const cormorant = Cormorant_Garamond({
-  variable: "--font-cormorant",
-  subsets: ["latin"],
-});
-
-const allura = Allura({
-  variable: "--font-allura",
-  subsets: ["latin"],
-  weight: "400",
-});
-
-const greatVibes = Great_Vibes({
-  variable: "--font-great-vibes",
-  subsets: ["latin"],
-  weight: "400",
-});
-
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Noelle & Nathan | Our Wedding",
-  description: "Join us as we celebrate our wedding.",
-};
-
-export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html
-      lang="en"
-      className={`${cormorant.variable} ${allura.variable} ${greatVibes.variable} ${montserrat.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
-        <LanguageProvider>
-          <WelcomeScreen />
-          <Navbar />
-          <LanguageSwitcher />
-          <MusicPlayerWrapper />
-          {children}
-        </LanguageProvider>
-      </body>
-    </html>
-  );
+  return {
+    title: isVietnameseSite
+      ? "Tấn Cường & Lãm Nghi | Save the Date"
+      : "Noelle & Nathan | Save the Date",
+    description: "Join us as we celebrate our wedding.",
+  };
 }
