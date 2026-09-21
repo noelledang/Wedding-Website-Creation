3dce61a742d2af22b73f184a2bdbaef275578953
Noelle Dang <dang.noelle@gmail.com>
2026-09-21 12:26:49 -0700

    Fix production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 
 import WelcomeScreen from "./WelcomeScreen";
-import RoseOverlay from "./components/RoseOverlay";
 import type { Metadata } from "next";
 import { headers } from "next/headers";
 import {
@@ -67,7 +66,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <MusicPlayerWrapper />
           {children}
         </LanguageProvider>
-        <RoseOverlay />
       </body>
     </html>
   );
